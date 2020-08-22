cbc2b59100aa8149d2cb17ca46dda730c89c63d3
duyuqian <809009017@qq.com>
2020-08-22 22:54:26 +0800

    [duyuqian] feat-1:valid the username and password is legal

--- app/src/main/java/com/duyuqian/todolist/LoginActivity.java
+++ app/src/main/java/com/duyuqian/todolist/LoginActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
+import butterknife.BindString;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnFocusChange;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.user_name)
+    EditText userName;
+    @BindView(R.id.password)
+    EditText passWord;
+    @BindString(R.string.valid_user_name)
+    String validUserName;
+    @BindString(R.string.valid_password)
+    String validPassWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +37,30 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
         startActivity(intent);
     }
+
+    @OnFocusChange({R.id.user_name, R.id.password})
+    public void setOnFocusChangeError(EditText view, boolean focused) {
+        String input = view.getText().toString();
+        switch (view.getId()) {
+            case R.id.user_name:
+                if (!focused) {
+                    String pattern = "^[a-zA-Z0-9]{3,12}$";
+                    if (!Pattern.compile(pattern).matcher(input).matches()) {
+                        view.setError(validUserName);
+                    }
+                }
+                break;
+            case R.id.password:
+                if (!focused) {
+                    String pattern = "[\\s\\S]{6,18}";
+                    if (!Pattern.compile(pattern).matcher(input).matches()) {
+                        view.setError(validPassWord);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
 }

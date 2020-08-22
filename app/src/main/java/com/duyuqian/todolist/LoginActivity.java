57ad5b88fd11bcbdfeb1061db9c59b929bd3e202
duyuqian <809009017@qq.com>
2020-08-22 22:14:09 +0800

    [duyuqian] feat-1:skip to Home page when click the login button

--- app/src/main/java/com/duyuqian/todolist/LoginActivity.java
+++ app/src/main/java/com/duyuqian/todolist/LoginActivity.java
@@ -2,7 +2,11 @@ package com.duyuqian.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.log_in_btn)
+    public void onClick() {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }

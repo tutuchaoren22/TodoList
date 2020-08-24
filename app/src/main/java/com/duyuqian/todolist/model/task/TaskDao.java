168df4b95cb89b5813e65999325cc88e790fc9f7
duyuqian <809009017@qq.com>
2020-08-24 15:28:37 +0800

    [duyuqian] feat-3: create the TaskDao class

---
+++ app/src/main/java/com/duyuqian/todolist/model/task/TaskDao.java
@@ -0,0 +1,16 @@
+package com.duyuqian.todolist.model.task;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+    @Query("SELECT * FROM task")
+    List<Task> getAll();
+
+    @Insert
+    void insertAll(Task... tasks);
+}

72f0651e3a1c0e4efb9d3941da15108fa1c1d0c4
duyuqian <809009017@qq.com>
2020-08-24 14:56:06 +0800

    [duyuqian] feat-3: enabled the finish button only when pick the date and write the title

--- app/src/main/java/com/duyuqian/todolist/view/DetailActivity.java
+++ app/src/main/java/com/duyuqian/todolist/view/DetailActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.duyuqian.todolist.R;
@@ -16,10 +19,15 @@ import java.util.Calendar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class DetailActivity extends AppCompatActivity implements DatePicker.OnDateChangedListener {
     @BindView(R.id.date)
     TextView date;
+    @BindView(R.id.title_input)
+    EditText title;
+    @BindView(R.id.finish_button)
+    Button finishBtn;
 
     @OnClick(R.id.date)
     public void onClickDate() {
@@ -29,6 +37,7 @@ public class DetailActivity extends AppCompatActivity implements DatePicker.OnDa
             public void onClick(DialogInterface dialog, int which) {
                 String dateStr = String.format("%d年%d月%d日", year, month + 1, day);
                 date.setText(dateStr);
+                isDeadlineSet = true;
                 dialog.dismiss();
             }
         });
@@ -44,9 +53,25 @@ public class DetailActivity extends AppCompatActivity implements DatePicker.OnDa
         dialog.setView(dialogView);
         dialog.show();
         datePicker.init(year, month, day, this);
+        updateFinishBtn();
+    }
+
+    @OnClick(R.id.finish_button)
+    public void onClickFinishBtn() {
+        Log.e("TAG", "onClickFinishBtn: ");
+    }
+
+    @OnTextChanged(value = R.id.title_input, callback = OnTextChanged.Callback.TEXT_CHANGED)
+    void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (count > 0) {
+            isTitleSet = true;
+        }
+        updateFinishBtn();
     }
 
     private int year, month, day;
+    boolean isDeadlineSet = false;
+    boolean isTitleSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,14 +81,6 @@ public class DetailActivity extends AppCompatActivity implements DatePicker.OnDa
         updateDate();
     }
 
-    private void updateDate() {
-        Calendar calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH) + 1;
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        String dateStr = String.format("%d年%d月%d日", year, month, day);
-        date.setText(dateStr);
-    }
 
     @Override
     protected void onResume() {
@@ -79,5 +96,22 @@ public class DetailActivity extends AppCompatActivity implements DatePicker.OnDa
         this.year = year;
         this.month = monthOfYear;
         this.day = dayOfMonth;
+    }
+
+    private void updateDate() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        String dateStr = String.format("%d年%d月%d日", year, month, day);
+        date.setText(dateStr);
+    }
+
+    private void updateFinishBtn() {
+        if (isDeadlineSet && isTitleSet) {
+            finishBtn.setEnabled(true);
+        } else {
+            finishBtn.setEnabled(false);
+        }
     }
 }

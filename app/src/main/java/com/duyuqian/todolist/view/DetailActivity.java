863607eaf8b9e23c76cb1b559868554d9e5c6f97
duyuqian <809009017@qq.com>
2020-08-24 13:53:40 +0800

    [duyuqian] feat-3: show the today date indefault

--- app/src/main/java/com/duyuqian/todolist/view/DetailActivity.java
+++ app/src/main/java/com/duyuqian/todolist/view/DetailActivity.java
@@ -4,15 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.duyuqian.todolist.R;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class DetailActivity extends AppCompatActivity {
+    @BindView(R.id.date)
+    TextView date;
+
+    @OnClick(R.id.date)
+    public void onClickDate() {
+
+    }
+
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+        updateDate();
+    }
+
+    private void updateDate() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        String dateStr = String.format("%d年%d月%d日", year, month, day);
+        date.setText(dateStr);
     }
 
     @Override
@@ -21,5 +48,8 @@ public class DetailActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
     }
+
+
 }

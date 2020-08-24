5b698db0d72d54714e5f339ff2b33444375b633e
duyuqian <809009017@qq.com>
2020-08-24 14:32:16 +0800

    [duyuqian] feat-3: dialog the date picker when click the date

--- app/src/main/java/com/duyuqian/todolist/view/DetailActivity.java
+++ app/src/main/java/com/duyuqian/todolist/view/DetailActivity.java
@@ -1,9 +1,12 @@
 package com.duyuqian.todolist.view;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.duyuqian.todolist.R;
@@ -14,13 +17,33 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements DatePicker.OnDateChangedListener {
     @BindView(R.id.date)
     TextView date;
 
     @OnClick(R.id.date)
     public void onClickDate() {
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String dateStr = String.format("%d年%d月%d日", year, month + 1, day);
+                date.setText(dateStr);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        final AlertDialog dialog = builder.create();
+        View dialogView = View.inflate(this, R.layout.dialog_date, null);
+        final DatePicker datePicker = dialogView.findViewById(R.id.date_picker);
+        dialog.setView(dialogView);
+        dialog.show();
+        datePicker.init(year, month, day, this);
     }
 
     private int year, month, day;
@@ -48,8 +71,13 @@ public class DetailActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
-
     }
 
 
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        this.year = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+    }
 }

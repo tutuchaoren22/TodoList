010a223768916ad4b15e5a861b3ea8ccbe51a281
duyuqian <809009017@qq.com>
2020-08-25 14:23:08 +0800

    [duyuqian] feat-2:add the Adapter of Task class to display the task in list view

---
+++ app/src/main/java/com/duyuqian/todolist/model/task/TaskAdapter.java
@@ -0,0 +1,58 @@
+package com.duyuqian.todolist.model.task;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.duyuqian.todolist.R;
+
+import java.util.List;
+
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+    private List<Task> taskList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CheckBox hasDone;
+        TextView title;
+        TextView date;
+
+        public ViewHolder(View view) {
+            super(view);
+            hasDone = view.findViewById(R.id.item_checkbox);
+            title = view.findViewById(R.id.item_title);
+            date = view.findViewById(R.id.item_date);
+        }
+    }
+
+    public TaskAdapter(List<Task> tasks) {
+        taskList = tasks;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Task task = taskList.get(position);
+        holder.hasDone.setChecked(task.isHasDone());
+        holder.title.setText(task.getTitle());
+        holder.date.setText(task.getDateOfRemind());
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+
+
+}
